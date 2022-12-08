f1716c17d0bae103dbba16ae884402ddba5ae23d
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2022-12-08 19:39:35 -0300

    introducão-js

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,7 @@
+			console.log("oi mundooooo");
+			
+			var titulo = document.querySelector(".titulo");
+			console.log(titulo);
+			console.log(titulo.textContent)
+
+			titulo.textContent = "Aparecida Nutricionista"
