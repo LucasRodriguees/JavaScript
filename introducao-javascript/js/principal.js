092e5d00b43cdd5408f9c37eba4f54083e5b000b
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2022-12-09 21:12:03 -0300

    buscando dados do paciente

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,7 +1,25 @@
 			console.log("oi mundooooo");
 			
 			var titulo = document.querySelector(".titulo");
-			console.log(titulo);
-			console.log(titulo.textContent)
+			//console.log(titulo);
+			//console.log(titulo.textContent)
 
 			titulo.textContent = "Aparecida Nutricionista"
+
+			var paciente = document.querySelector("#primeiro-paciente");
+
+			var tdpeso = paciente.querySelector(".info-peso");
+
+			var tdaltura = paciente.querySelector(".info-altura");
+
+			var peso = tdpeso.textContent;
+
+			var altura = tdaltura.textContent;
+
+			var imc = peso / (altura * altura); // 100 / 2.0 x 2.0 = 100 / 4 = 25
+
+				// selecionando a coluna do IMC
+			var tdImc = document.querySelector(".info-imc");
+
+			tdImc.textContent = imc; 
+
