6edd4f792c517c66f3970eb385ff8492752dd398
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2022-12-13 19:01:39 -0300

    replicando validação e o cálculo do IMC

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,41 +1,47 @@
 			console.log("oi mundooooo");
 			
 			var titulo = document.querySelector(".titulo");
-			//console.log(titulo);
-			//console.log(titulo.textContent)
 
 			titulo.textContent = "Aparecida Nutricionista"
 
-			var paciente = document.querySelector("#primeiro-paciente");
+			var pacientes = document.querySelectorAll(".paciente");
 
-			var tdpeso = paciente.querySelector(".info-peso");
+			for(var i = 0; i < pacientes.length; i++){
 
-			var tdaltura = paciente.querySelector(".info-altura");
+					var paciente = pacientes[i];		
 
-			var peso = tdpeso.textContent;
+				var tdpeso = paciente.querySelector(".info-peso");
+				var peso = tdpeso.textContent;
 
-			var altura = tdaltura.textContent;
+				var tdaltura = paciente.querySelector(".info-altura");
+				var altura = tdaltura.textContent;
+				
+					// selecionando a coluna do IMC
+				var tdImc = paciente.querySelector(".info-imc");
 
-				// selecionando a coluna do IMC
-			var tdImc = document.querySelector(".info-imc");
+				var pesoEhValido = true;
+				var alturaEhValida = true;
 
-			var pesoEhValido = true;
-			var alturaEhValida = true;
-
-			if (peso <= 0 || peso >= 1000){
+				if (peso <= 0 || peso >= 1000){
 				console.log("peso inválido");
 				pesoEhValido = false;
-				tdImc.textContent = "peso inválido"
+				tdImc.textContent = "peso inválido";
+				paciente.classList.add("paciente-invalido");
 			}
 
-			if (altura <= 0 || altura >= 3.00){
+				if (altura <= 0 || altura >= 3.00){
 				console.log("altura inválida")
 				alturaEhValida = false;
 				tdImc.textContent = "altura inválida"
+				paciente.classList.add("paciente-invalido");
+
 			}
 
-			if(pesoEhValido && alturaEhValida){
+				if(pesoEhValido && alturaEhValida){
 				var imc = peso / (altura * altura); 
-				tdImc.textContent = imc; 
+				tdImc.textContent = imc.toFixed(2); 
 			}
+			
+			}
+
 			
