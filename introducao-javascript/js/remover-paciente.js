b654ee21b75c902f8084ebcb233facd48b725fd1
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2022-12-22 19:32:24 -0300

    Animando a remoção do paciente

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -3,9 +3,11 @@
 		var tabela = document.querySelector("table");
 		
 			tabela.addEventListener("dblclick", function(event){
-				var alvoEvento = event.target;
-				var paiDoAlvo = alvoEvento.parentNode;
-					paiDoAlvo.remove();
+				event.target.parentNode.classList.add("fadeOut");
+
+				setTimeout(function(){
+					event.target.parentNode.remove();
+				},500);
 
 			});
 
