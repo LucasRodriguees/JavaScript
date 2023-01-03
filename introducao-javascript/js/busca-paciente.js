15570fcc87d675f43e7b6084d8f9e44be6590dbe
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2023-01-03 20:43:31 -0300

    capturando erros de requisição

--- introducao-javascript/js/busca-paciente.js
+++ introducao-javascript/js/busca-paciente.js
@@ -5,14 +5,22 @@ botaoAdicionar.addEventListener("click", function(){
 
 	xhr.open("GET", "https://raw.githubusercontent.com/mmgcnerds/api-pacientes/main/api-pacientes.json");
 
-		xhr.addEventListener("load", function(){
-			var resposta = xhr.responseText;
 
-			var pacientes = JSON.parse(resposta);
+		xhr.addEventListener("load", function(){
+			var erroAjax = document.querySelector("#erro-ajax");
+			if(xhr.status == 200){
+				var resposta = xhr.responseText;
+				var pacientes = JSON.parse(resposta);
 			
-			pacientes.forEach(function (paciente){
+				pacientes.forEach(function (paciente){
 				adicionaPacienteNaTabela(paciente);
 			}); 
+		}else{
+			erroAjax.classList.remove("invisivel");
+			console.log(xhr.status);
+			console.log(xhr.responseText);
+			
+			}
 		});
 
 		xhr.send();
