ef2240ebd10cb435b11d3586302802d0db17c542
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2023-01-04 22:27:39 -0300

    Guardando Valores na memória do computador

---
+++ int-js/1-meuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+console.log("Meu Primeiro programa:Trabalhando com variaveis");
+
+idade = 21;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+console.log(idade*2);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
