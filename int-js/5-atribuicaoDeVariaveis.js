a1883e364979a8be2a72bee311ec9e1938b1c243
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2023-01-09 19:15:56 -0300

    Atribuição e imutabilidade

---
+++ int-js/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const primeiroNome = "Ricardo";
+const sobrenome = "bugan";
+
+console.log(primeiroNome, sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
