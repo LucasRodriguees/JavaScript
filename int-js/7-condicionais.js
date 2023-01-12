6c973c66e8fa26f1c2065126f38ffbf3d59bcf5b
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2023-01-12 19:26:19 -0300

    Operadores lógicos encadeados

--- int-js/7-condicionais.js
+++ int-js/7-condicionais.js
@@ -1,5 +1,7 @@
 
 const idadeComprador = 18;
+const estaAcompanhada = false;
+const temPassagemComprada = true;
 console.log(`Destinos possíveis`);
 
 const listaDeDestinos = new Array(
@@ -7,19 +9,23 @@ const listaDeDestinos = new Array(
     `São Paulo`,
     `Rio de Janeiro`
 );
+console.log(listaDeDestinos);
 
-if(idadeComprador >= 18){
+if(idadeComprador >= 18 || estaAcompanhada == true) {
     console.log("Comprador maior de idade");
     listaDeDestinos.splice(1,1);
-}else{
-    console.log("Comprador menor de idade");
+} else {
+    console.log("Comprador menor de idade não posso vender");
 }
 
-console.log(listaDeDestinos);
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador == 18);
+console.log("Embarque: \n\n");
+if (temPassagemComprada && idadeComprador >= 18){
+    console.log("Boa viagem");
+} else {
+    console.log("Você não pode embarcar");
+}
+    
+ console.log(listaDeDestinos);
+
 
 
