138923de4f98ea14b54a62eb45571193e005690d
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2023-02-20 16:38:37 -0300

    Métodos com retorno

--- Orientacao a Objetos-Js/index.js
+++ Orientacao a Objetos-Js/index.js
@@ -12,13 +12,15 @@ class ContaCorrente{
     sacar(valor){
         if(this._saldo >= valor){
             this._saldo -= valor;
+            return valor;
         }
     }
 
     depositar(valor){
-        if(valor > 0){
-            this._saldo += valor;
+        if(valor <= 0){
+            return;
         }
+        this._saldo += valor;
     }
 }
 
@@ -37,7 +39,8 @@ contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.depositar(100);
 
-contaCorrenteRicardo.sacar(50);
+const valorSacado = contaCorrenteRicardo.sacar(50);
 
+console.log(valorSacado);
 console.log(contaCorrenteRicardo); 
 
