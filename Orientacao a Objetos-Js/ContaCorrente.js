2dddf7bd8764442c19e917cd20b5e70600c5c80f
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2023-03-01 21:19:11 -0300

    Relembrando o projeto

--- Orientacao a Objetos-Js/ContaCorrente.js
+++ Orientacao a Objetos-Js/ContaCorrente.js
@@ -1,12 +1,7 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
-    agencia;
    static numeroDeContas = 0;
-
-    _cliente;
-    _saldo = 0;
-    
 
     set cliente(novoValor){
         if(novoValor instanceof Cliente){
@@ -46,6 +41,7 @@ export class ContaCorrente{
     constructor(agencia,cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
 }
--- Orientacao a Objetos-Js/index.js
+++ Orientacao a Objetos-Js/index.js
@@ -1,17 +1,16 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Ricardo",11122233309);
-const cliente2 = new Cliente("Aline",88822233308);
+
 const contaCorrenteRicardo = new ContaCorrente(1001,cliente1);
 contaCorrenteRicardo.depositar(500);
 
-const contaCorrente2 = new ContaCorrente(2002,cliente2);
+const contaPoupanca = new ContaPoupanca(50,cliente1,1001);
 
-let valor = 200;
-contaCorrenteRicardo.transferir(valor, contaCorrente2);
-
-console.log(ContaCorrente.numeroDeContas);
+console.log(contaPoupanca);
+console.log(contaCorrenteRicardo);
 
 
 
