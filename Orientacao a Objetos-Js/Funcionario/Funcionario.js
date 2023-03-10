bd9228684cc1c76e7195975b51b60f79a47ed8d5
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2023-03-10 18:54:44 -0300

    Polimorfismo

--- Orientacao a Objetos-Js/Funcionario/Funcionario.js
+++ Orientacao a Objetos-Js/Funcionario/Funcionario.js
@@ -4,6 +4,15 @@ export class Funcionario{
         this._cpf;
         this._salario;
 
+        this._senha;
         this._bonificacao = 1;
+    }
+
+    get senha(){
+        return this._senha;
+    }
+
+    cadastrarSenha(senha){
+        this._senha = senha;
     }
 }
--- Orientacao a Objetos-Js/Funcionario/Gerente.js
+++ Orientacao a Objetos-Js/Funcionario/Gerente.js
@@ -4,5 +4,9 @@ export class Gerente extends Funcionario {
     constructor(nome, salario, cpf) {
         super(nome, salario, cpf);
         this._bonificacao = 1.1;
+       
+
     }
+
+    
 }
