8014b3c1ffbda8d0abe23f5e4856d41203f131e9
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2023-03-31 19:15:51 -0300

    Template string e expressão regular

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,6 +1,11 @@
 class DateHelper{
 
-    textoParaData(texto){
+
+    static textoParaData(texto){
+
+        if(!/^\d{4}-\d{2}-\d{2}/.test(texto))
+        throw new Error ("Deve estar no formato aaaa-mm-dd");
+
        return new Date(
             ...texto
             .split("-")
@@ -8,10 +13,10 @@ class DateHelper{
         );
     }
     
-    dataParaTexto(data){
-             return data.getDate() 
-        + "/" + (data.getMonth() + 1)
-        + "/" +  data.getFullYear();
+    static dataParaTexto(data){
+
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`
+              
     }
 
 }
