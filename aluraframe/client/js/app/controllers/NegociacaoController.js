d59bdcc05baa55974fd62e97dd6be1304367b73c
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2023-03-30 21:43:30 -0300

    isolando a responsabolidade de conversão de datas

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -11,18 +11,18 @@ class NegociacaoController{
 
         event.preventDefault();
 
-        let data = new Date(
-            ...this._inputData.value
-            .split("-")
-            .map((item, indice) => item- indice % 2)  
-
-             );
+        let helper = new DateHelper();
+        let data = helper.textoParaData(this._inputData.value);
+            
         let negociacao = new Negociacao(
             data,
             this._inputQuantidade.value,
             this._inputValor.value
         );
+        
         console.log(negociacao);
+        console.log(helper.dataParaTexto(negociacao.data));
+       
     }
  
 }
