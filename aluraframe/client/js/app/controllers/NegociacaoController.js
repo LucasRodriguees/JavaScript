53ae343ea228a9d33879b5a45f3c28de2df534a4
Lucas Rodrigues <lucas.orodrigues@hotmail.com>
2023-04-12 20:55:29 -0300

    criando a classe MensagemView

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,25 +2,32 @@ class NegociacaoController{
 
     constructor(){
         let $ = document.querySelector.bind(document);
-         this._inputData = $("#data"); 
-         this._inputQuantidade = $("#quantidade");
-         this._inputValor = $("#valor");
-         this._listaNegociacoes = new ListaNegociacoes();
-         this._negociacoesView = new NegociacoesView($("#NegociacoesView"));
+            this._inputData = $("#data"); 
+            this._inputQuantidade = $("#quantidade");
+            this._inputValor = $("#valor");
+            this._listaNegociacoes = new ListaNegociacoes();
 
-         this._negociacoesView.update(this._listaNegociacoes);
+            this._negociacoesView = new NegociacoesView($("#NegociacoesView"));
+            this._negociacoesView.update(this._listaNegociacoes);
 
+            this._mensagem = new Mensagem();
+            this._mensagemView = new MensagemView($("#mensagemView"));
+            this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event){
 
         event.preventDefault();
         
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
-        this._limpaFormulario();
+            this._listaNegociacoes.adiciona(this._criaNegociacao());
+            this._negociacoesView.update(this._listaNegociacoes);
 
-        console.log(this._listaNegociacoes.negociacoes);
+            this._mensagem.texto = "Negociação adicionada com sucesso";
+            this._mensagemView.update(this._mensagem);
+
+            this._limpaFormulario();
+
+            console.log(this._listaNegociacoes.negociacoes);
        
     }
 
@@ -33,11 +40,11 @@ class NegociacaoController{
     }
 
     _limpaFormulario(){
-        this._inputData.value = "";
-        this._inputQuantidade.value = 1;
-        this._inputValor.value = 0.0;
+            this._inputData.value = "";
+            this._inputQuantidade.value = 1;
+            this._inputValor.value = 0.0;
 
-        this._inputData.focus();
+            this._inputData.focus();
     }
 
 
